f67c9e1684ef1ea813f175978bab37a548719c6f
Linda Hurtig <lindahurtig93@gmail.com>
2024-01-11 14:31:42 +0100

    JSON databas and HTTP communication

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -14,7 +14,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
     class="listing-photo"
     [src]="housingLocation.photo"
     alt="Exterior photo of {{ housingLocation.name}}"
-    crossorigin
     />
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
     <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state}}</p>
