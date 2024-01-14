23298886689e43d81d9d33d04a4002f037a87e3c
Linda Hurtig <lindahurtig93@gmail.com>
2024-01-14 22:46:15 +0100

    fix med json server

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -2,26 +2,28 @@ import { Injectable } from '@angular/core';
 import { HousingLocation } from './housinglocation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
-
   readonly url = 'http://localhost:3000/locations';
-
-  
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
 
   async getAllHousingLocations(): Promise<HousingLocation[]> {
-   const data = await fetch(this.url);
-    return await data.json() ?? [];
+    const data = await fetch(this.url);
+    console.log({ data });
+    return (await data.json()) ?? [];
   }
 
-  async getHousingLocationsById(id: Number): Promise<HousingLocation | undefined>{
+  async getHousingLocationsById(
+    id: Number
+  ): Promise<HousingLocation | undefined> {
     const data = await fetch(`${this.url}/${id}`);
-    return await data.json() ?? {};
+    console.log(`${this.url}/${id}`);
+    return (await data.json()) ?? {};
   }
 
-  submitApplication (firstName: string, lastName: string, email: string){
+  submitApplication(firstName: string, lastName: string, email: string) {
     console.log(firstName, lastName, email);
   }
-
 }
